6dc321e13172e4138b91802e20d51d6f1c7d1759
Felipe Paixão <fleap@fleap.dev>
2024-10-14 10:09:46 -0300

    feat: implement time function

--- dsl-core/src/runtime/function/defaults.rs
+++ dsl-core/src/runtime/function/defaults.rs
@@ -1,7 +1,9 @@
 use crate::runtime::function::Function;
 use crate::runtime::value::Value;
+use anyhow::{bail, Context};
+use chrono::{NaiveTime, Utc};
 use lazy_static::lazy_static;
-use tracing::info;
+use tracing::{debug, info};
 
 lazy_static! {
     pub static ref DEFAULT_FUNCTIONS: Vec<Function> = {
@@ -33,6 +35,37 @@ lazy_static! {
             Function {
                 name: "event_time".to_owned(),
                 fun: |ctx, _args| Ok(ctx.event.datetime.time().into()),
+            },
+            Function {
+                name: "time".to_owned(),
+                fun: |_ctx, args| {
+                    let arg = args.first();
+                    match arg {
+                        Some(arg) => {
+                            if let Value::String(s) = arg {
+                                let mut parts = s.split(":");
+                                let hours: u32 = parts.next()
+                                    .context("time string is empty")?
+                                    .parse()
+                                    .context("failed to parse hours")?;
+                                let mins: u32 = parts.next()
+                                    .map(|s| s.parse::<u32>())
+                                    .unwrap_or(Ok(0))
+                                    .context("failed to parse minutes")?;
+                                let secs: u32 = parts.next()
+                                    .map(|s| s.parse::<u32>())
+                                    .unwrap_or(Ok(0))
+                                    .context("failed to parse seconds")?;
+                                Ok(Value::Time(NaiveTime::from_hms_opt(hours, mins, secs).context("invalid time provided")?))
+                            } else {
+                                bail!("time function only accepts strings");
+                            }
+                        },
+                        None => {
+                            Ok(Value::Time(Utc::now().time()))
+                        }
+                    }
+                },
             },
         ]
     };
