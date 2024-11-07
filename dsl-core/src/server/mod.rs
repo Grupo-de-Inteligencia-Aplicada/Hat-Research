52d2f5c2b532e3f96e52a5993d05d8fea7e9b6a2
Felipe Paixão <fleap@fleap.dev>
2024-11-07 12:34:19 -0300

    feat: pass the runtime to the router as State

--- dsl-core/src/server/mod.rs
+++ dsl-core/src/server/mod.rs
@@ -4,8 +4,15 @@ use axum::Router;
 
 use crate::runtime::HatRuntime;
 
+struct AppState {
+    runtime: Arc<HatRuntime>,
+}
+
 pub fn make_router(runtime: Arc<HatRuntime>) -> Router {
-    let router = Router::new();
+    let router = Router::new()
+        .with_state(AppState {
+            runtime
+        });
 
     router
 }
